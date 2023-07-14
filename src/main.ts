022fd7edc980be536372e46bc583541ab1f9399a
ioott <vioott@gmail.com>
2023-07-14 10:53:51 -0300

    build: adding new settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { config } from 'dotenv';
+
+const PORT = process.env.PORT || 3001;
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
