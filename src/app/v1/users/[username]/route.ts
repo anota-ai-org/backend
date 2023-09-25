12519f0d3e01bcf33319ff4ac82581935f125bcf
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-09-25 10:04:26 -0300

    fix: add basic endpoint structure to user/[username]

--- src/app/v1/users/[username]/route.ts
+++ src/app/v1/users/[username]/route.ts
@@ -1,1 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
 // TODO: create endpoint that gets username from url and returns user data
+export async function GET(req: NextRequest) {
+  const response = new NextResponse(JSON.stringify({ status: "success" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  return response;
+}
